41928b3f603ee90101805b8b4e56cb07f6d75034
Maycol Carroll <carrollmaycol@gmail.com>
2022-10-14 14:27:56 +0200

    Clase 14/10

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,24 @@
 
 import './App.css';
 import React from 'react';
+import Header from './components/layout/Header';
+import { Footer } from './components/layout/Footer';
 
 
 function App() {
   return (
     <div className="App">
-        <h1>Tienda Nativo Style</h1>
-        <h2>HandMade</h2>
-        <img src="./images/nativo.png" alt="Aqui va el logo"></img>
+        <Header />
+        <center>Contenido aqui</center>
+        <center>Contenido aqui</center>
+        <center>Contenido aqui</center>
+        <center>Contenido aqui</center>
+        <center>Contenido aqui</center>
+        <center>Contenido aqui</center>
+        <center>Contenido aqui</center>
+        <center>Contenido aqui</center>
+        <center>Contenido aqui</center>
+        <Footer />
     </div>
   );
 }
