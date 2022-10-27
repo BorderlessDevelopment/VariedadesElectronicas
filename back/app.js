ca642dca7584e06df282efec62d8a11fa39e1526
Maycol Carroll <carrollmaycol@gmail.com>
2022-10-27 22:31:39 +0200

    Clase 27/10

--- back/app.js
+++ back/app.js
@@ -1,12 +1,17 @@
-const express = require("express");
+const express=require("express");
 const app = express();
+const errorMiddleware= require("./middleware/errors")
 
 app.use(express.json());
 
-//Importar rutas 
-const productos= require("./routes/products")
+//Importar rutas
+const productos=require("./routes/products")
+const usuarios=require("./routes/auth")
 
-app.use("/api",productos) //sujeto a decisión (ruta del navegador)
+app.use('/api',productos) //Sujeto a decision (ruta del navegador)
+app.use('/api',usuarios)
 
+//MiddleWares para manejar errores
+app.use(errorMiddleware)
 
-module.exports = app;
+module.exports=app
