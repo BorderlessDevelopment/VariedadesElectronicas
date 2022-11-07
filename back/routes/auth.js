21625b27f609e6d007a99f78357c5a1cf4c9136e
Maycol Carroll <carrollmaycol@gmail.com>
2022-11-07 19:53:22 +0100

    Actualizado 07/11/2022

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,6 +1,18 @@
 const express=require("express");
-const { registroUsuario, loginUser, logOut, forgotPassword, resetPassword } = require("../controllers/authController");
-const { isAuthenticatedUser } = require("../middleware/auth");
+const { registroUsuario, 
+    loginUser, 
+    logOut, 
+    forgotPassword, 
+    resetPassword, 
+    getUserProfile, 
+    updatePassword,
+    updateProfile,
+    getAllUsers,
+    getUserDetails,
+    updateUser,
+    deleteUser
+} = require("../controllers/authController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router= express.Router();
 
 
@@ -9,5 +21,14 @@ router.route('/login').get(loginUser)
 router.route('/logout').get(isAuthenticatedUser, logOut)
 router.route("/forgotPassword").post(forgotPassword)
 router.route('/resetPassword/:token').post(resetPassword)
+router.route('/logueado').get(isAuthenticatedUser, getUserProfile)
+router.route('/logueado/updatePassword').put(isAuthenticatedUser, updatePassword)
+router.route('/logueado/updateProfile').put(isAuthenticatedUser, updateProfile)
+router.route("/admin/deleteUser/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser)
+
+//rutas admin
+router.route('/admin/allUsers').get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers)
+router.route('/admin/user/:id').get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails)
+router.route('/admin/updateUser/:id').put(isAuthenticatedUser, authorizeRoles("admin"), updateUser)
 
 module.exports= router
